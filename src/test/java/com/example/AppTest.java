95e1df46aba2afdd8a85e95d8a32b2e38a327204
Avinash Avinash <avinash270505@gmail.com>
2026-09-23 12:50:38 +0530

    Initial Maven project with JUnit test

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,13 +1,13 @@
 package com.example;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class AppTest {
 
     @Test
     public void testAdd() {
         App app = new App();
-        assertEquals(5, app.add(2, 3));
+        assertEquals(8, app.add(5, 3));
     }
 }
